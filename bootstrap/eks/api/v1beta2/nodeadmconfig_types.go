73d38ca5f7003139d563287c3c661b6f6605b787
Aidan Rowe <aidan@canva.com>
2026-01-08 12:30:20 +1100

    feat: extend nodeadmconfig types to cover hybrid node configuration options

--- bootstrap/eks/api/v1beta2/nodeadmconfig_types.go
+++ bootstrap/eks/api/v1beta2/nodeadmconfig_types.go
@@ -44,6 +44,84 @@ type NodeadmConfigSpec struct {
 	// Mounts specifies a list of mount points to be setup.
 	// +optional
 	Mounts []MountPoints `json:"mounts,omitempty"`
+
+	// Hybrid contains configuration for EKS Hybrid Nodes.
+	// When specified, the NodeadmConfig generates userdata for hybrid node
+	// bootstrapping instead of standard EC2 node bootstrapping.
+	// Hybrid mode is mutually exclusive with EC2-specific options.
+	// +optional
+	Hybrid *HybridOptions `json:"hybrid,omitempty"`
+}
+
+// HybridOptions defines configuration for EKS Hybrid Nodes.
+// When specified, the NodeadmConfig generates userdata for hybrid node
+// bootstrapping instead of standard EC2 node bootstrapping.
+type HybridOptions struct {
+	// SSM configures SSM-based authentication for hybrid nodes.
+	// This is required for hybrid node support.
+	// +kubebuilder:validation:Required
+	SSM *HybridSSMOptions `json:"ssm"`
+}
+
+// HybridSSMOptions configures SSM activation-based authentication for hybrid nodes.
+// Either ActivationRef or ActivationConfig must be specified, but not both.
+type HybridSSMOptions struct {
+	// ActivationRef references an existing Secret containing SSM activation credentials.
+	// The Secret must contain 'activationId' and 'activationCode' keys.
+	// When specified, the controller will not create or manage SSM activations.
+	// +optional
+	ActivationRef *SSMActivationReference `json:"activationRef,omitempty"`
+
+	// ActivationConfig specifies parameters for automatically creating an SSM activation.
+	// The controller will create the activation and store credentials in a Secret.
+	// The activation will be deleted when the NodeadmConfig is deleted.
+	// +optional
+	ActivationConfig *SSMActivationConfig `json:"activationConfig,omitempty"`
+}
+
+// SSMActivationReference references a Secret containing pre-created SSM activation credentials.
+type SSMActivationReference struct {
+	// Name is the name of the Secret in the same namespace as the NodeadmConfig.
+	// The Secret must contain 'activationId' and 'activationCode' keys.
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:MinLength=1
+	Name string `json:"name"`
+}
+
+// SSMActivationConfig specifies parameters for creating an SSM hybrid activation.
+type SSMActivationConfig struct {
+	// IAMRoleARN is the ARN of the IAM role that hybrid nodes will assume.
+	// This role must have the necessary permissions for EKS hybrid nodes
+	// and trust policy allowing ssm.amazonaws.com to assume it.
+	// See: https://docs.aws.amazon.com/eks/latest/userguide/hybrid-nodes-creds.html
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Pattern=`^arn:aws(-[a-z]+)?:iam::[0-9]{12}:role/.+$`
+	IAMRoleARN string `json:"iamRoleARN"`
+
+	// RegistrationLimit is the maximum number of hybrid nodes that can register
+	// using this activation. Each NodeadmConfig creates its own activation,
+	// so this typically corresponds to the number of nodes using this config.
+	// Minimum: 1, Maximum: 1000, Default: 1
+	// +kubebuilder:default=1
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=1000
+	// +optional
+	RegistrationLimit *int32 `json:"registrationLimit,omitempty"`
+
+	// ExpirationDays is the number of days until the activation expires.
+	// After expiration, no new nodes can register using this activation,
+	// but already-registered nodes are unaffected.
+	// Minimum: 1, Maximum: 30, Default: 7
+	// +kubebuilder:default=7
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=30
+	// +optional
+	ExpirationDays *int32 `json:"expirationDays,omitempty"`
+
+	// Tags are key-value pairs to apply to the SSM activation.
+	// These tags help identify and manage activations in the AWS console.
+	// +optional
+	Tags map[string]string `json:"tags,omitempty"`
 }
 
 // KubeletOptions are additional parameters passed to kubelet.
@@ -116,6 +194,26 @@ type NodeadmConfigStatus struct {
 	// Conditions defines current service state of the NodeadmConfig.
 	// +optional
 	Conditions clusterv1beta1.Conditions `json:"conditions,omitempty"`
+
+	// SSMActivation contains status information about the SSM activation
+	// when hybrid mode is enabled with auto-created activations.
+	// +optional
+	SSMActivation *SSMActivationStatus `json:"ssmActivation,omitempty"`
+}
+
+// SSMActivationStatus contains status information for an auto-created SSM activation.
+type SSMActivationStatus struct {
+	// ActivationID is the ID of the SSM activation.
+	// +optional
+	ActivationID *string `json:"activationID,omitempty"`
+
+	// SecretName is the name of the Secret containing activation credentials.
+	// +optional
+	SecretName *string `json:"secretName,omitempty"`
+
+	// ExpirationTime is when the activation expires.
+	// +optional
+	ExpirationTime *metav1.Time `json:"expirationTime,omitempty"`
 }
 
 // +kubebuilder:object:root=true
